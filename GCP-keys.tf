4e53326a31f5afbf17b486dc20b2444c3803f15c
marufhossainergo <md.maruf.hossain@ergo.com>
2026-03-16 16:00:35 +0100

    Create GCP-keys.tf

---
+++ GCP-keys.tf
@@ -0,0 +1,10 @@
+{
+  "type": "service_account",
+  "project_id": "demo-project-123",
+  "private_key_id": "abc123fakekeyid987654",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvFAKEKEYEXAMPLE1234567890abcdefg\n-----END PRIVATE KEY-----\n",
+  "client_email": "demo-service-account@demo-project-123.iam.gserviceaccount.com",
+  "client_id": "109876543210987654321",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token"
+}
